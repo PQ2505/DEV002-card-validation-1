8f7dde4a53b632bdcefb7c47d2f95950aa3a20f8
Paola Quiroga <paolaquirogap10@gmail.com>
2022-11-03 19:12:28 -0500

    Repo listo con proyecto CV

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import validator from './validator.js';
 
+//obtener el input (donde el usuario va a escribir)
+const inputCard = document.getElementById ("numeroTarjeta")
+const labelResult = document.getElementById ("respuesta")
+const inputName = document.getElementById ("nombreUsuario")
+const labelName = document.getElementById ("nombre")
+const labelNumero = document.getElementById ("numero")
+const imglogos = document.getElementById ("franchise")
+
+console.log(inputCard)
+
+//al input asignarle un evento para detectar cada vez que cambie
+inputCard.addEventListener('input', function (evento) {
+    var creditCardNumber = evento.target.value;
+    labelNumero.innerText = validator.maskify(creditCardNumber);
+    var img = validator.franchise(creditCardNumber);
+    imglogos.src = "Imagenes/" + img;
+    var valid = validator.isValid(creditCardNumber);
+    if (valid) {
+        labelResult.innerText = "TARJETA VALIDA"
+    } else {
+        labelResult.innerText = "TARJETA INVALIDA"
+    }
+});
+inputName.addEventListener('input', function (evento) {
+    var name = evento.target.value;
+    labelName.innerText = (name + "").toUpperCase();
+});
+
+
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,4 +1,46 @@
 const validator = {
+  isValid(creditCardNumber) {
+    const arrayNumber = creditCardNumber.toString().split('').reverse();
+    if (arrayNumber.length > 16) {
+      return false
+      }
+    for (let index = 1; index < arrayNumber.length; index+=2) {
+      var number = arrayNumber[index];
+      number = number * 2;
+      if (number >= 10) {
+        var arraySum = number.toString().split('');
+        number = parseInt(arraySum[0]) + parseInt(arraySum[1]);
+      }
+      arrayNumber[index] = number;
+    }
+    var result = 0;
+    for (let index = 0; index < arrayNumber.length; index++) {
+      result = result + parseInt(arrayNumber[index]);
+    }
+    if (result%10==0) {
+      return true;  
+    }
+    return false;
+  },
+  maskify(creditCardNumber) {
+    const arrayNumber = creditCardNumber.toString().split('');
+    for (let index = 0; index<arrayNumber.length-4 ; index++) {
+      arrayNumber[index] = "#";
+    }
+    return arrayNumber.join('');
+  },
+  franchise (creditCardNumber) {
+    const arrayNumber = creditCardNumber.toString().split('');
+    if (arrayNumber[0] === '4'){
+      return "logo_visa.png";
+    } else if (arrayNumber[0] === '5') {
+      return "logo_mastercard.png"
+    } else { 
+      alert("Solo tarjetas VISA o MASTERCARD")
+    }
+
+  }
+
   // ...
 };
 
